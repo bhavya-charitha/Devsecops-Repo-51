c7f0800887ee0b4d7001995671c7206bc1b6a538
bhavya charitha <vedcheritha@gmail.com>
2023-02-15 12:43:40 +0530

    JIRA 1012

--- HSBC-LIFE-DIGITALAPPS/src/main/java/jar/App.java
+++ HSBC-LIFE-DIGITALAPPS/src/main/java/jar/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "change from DEV02" );
-        System.out.println( "change from DEV01" );
+        System.out.println( "this is devops class" );
 		System.out.println( "user-story-1" );
     }
 }
